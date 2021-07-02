9b64a2bad1fcb48071e9467b9301d5e7639f71d4
Dmitry Kucherenko <fatalzero@mail.ru>
2021-07-02 22:17:53 +0300

    feature:checkChooseFromArray

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -1,9 +1,21 @@
 package subtask1
 
+
 class Combinator {
 
-    // TODO: Complete the following function
+    private fun factorial(n: Int): Double = if (n < 2) 1.0 else n * factorial(n - 1)
+    private fun binominal(n:Int, x:Int):Double=factorial(n)/(factorial(x)*factorial(n-x))
+
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        val m=array[0]
+        val n=array[1]
+        if(m<n||n<=0||m<0)return null
+        for(x in 1.. n){
+            if(m-binominal(n,x)==0.0)
+           return x
+        }
+        return null
     }
 }
+
+
