e87e9e41fd4591d160c9208a856b69eefa2fd572
Dmitry Kucherenko <fatalzero@mail.ru>
2021-07-04 18:11:28 +0300

    feature:SquareDecomposer

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -1,3 +1,4 @@
+
 package subtask1
 
 
--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -1,9 +1,57 @@
 package subtask2
 
-class SquareDecomposer {
+import java.util.*
+import kotlin.math.sqrt
 
-    // TODO: Complete the following function
-    fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+
+class SquareDecomposer {
+    fun squre(x:Int):Int{
+        return x*x
     }
+    fun decomposeNumber(number: Int):  Array<Int>? {
+        var ad= ArrayDeque<Int>()
+        var map= mutableMapOf<Int,Int>()
+        var value = number-1
+        var squareValue = squre(number)
+        for(i in value downTo 1){
+            var ss = squareValue-squre(value)
+            map.put(squareValue,value)
+            ad.push(ss)
+            while(!ad.isEmpty()){
+                var xx = ad.peek()
+                var b=0
+                if(map.containsKey(xx)){
+                    b= map[xx]!!-1
+                    if(b<3){
+                        ad.pop()
+                        map.remove(xx)
+                        continue
+                    }
+                }   else b= sqrt(xx.toDouble()).toInt()
+
+                if(map.containsValue(b)){
+                    ad.pop()
+
+                }else
+                    if(xx-b*b==0) {
+                        map.put(xx,b)
+                        return map.values.toTypedArray().sortedArray()
+                    }
+                     else{
+                        map.put(xx,b)
+                        ad.push(xx-b*b)}
+            }
+        }
+        return null
+    }
+}
+
+
+
+fun main(){
+ // println(isInteger(4))
+    var sq = SquareDecomposer()
+    println( sq.decomposeNumber(2))
+    sq.decomposeNumber(-50)?.forEach { println(it) }
+
 }
